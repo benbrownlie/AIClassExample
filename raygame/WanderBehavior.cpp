b45d9b32abfc2260a67ac12c2d0c6e95672fb565
Ben Brownlie <benjibspam@gmail.com>
2021-03-12 11:58:11 -0600

    Began redoing Wander Behavior
    Started on redefining calculateForce function and WanderBehavior constructor

--- raygame/WanderBehavior.cpp
+++ raygame/WanderBehavior.cpp
@@ -6,23 +6,33 @@ WanderBehavior::WanderBehavior()
 	setForceScale(1);
 }
 
-WanderBehavior::WanderBehavior(Agent* target, float wanderForce)
+WanderBehavior::WanderBehavior(float circleDistance, float circleRadius, float wanderForce)
 {
-	m_target = target;
+	m_circleDistance = circleDistance;
+	m_circleRadius = circleRadius;
 	setForceScale(wanderForce);
 }
 
 MathLibrary::Vector2 WanderBehavior::calculateForce(Agent* agent)
 {
-	//Find the direction to move in
-	MathLibrary::Vector2 direction = MathLibrary::Vector2::normalize(m_target->getWorldPosition() - agent->getWorldPosition());
-	//Scale the direction vector by the wanderForce
-	MathLibrary::Vector2 desiredVelocity = direction * getForceScale();
-	//Find the circle
-	MathLibrary::Vector2 circleCenter = desiredVelocity;
-	circleCenter = circleCenter.normalize(circleCenter);
+	//Find the agents facing
+	MathLibrary::Vector2 circleCenter = agent->getForward() * m_circleDistance;
 
+	//Find the direction and set it to a (vector2) random point in the circle with a x(cos) and y(sin)
+	MathLibrary::Vector2 ranDirection = MathLibrary::Vector2(cos(rand()), sin(rand()));
+
+	//direction * circleRadius
+	ranDirection = ranDirection * m_circleRadius;
+
+	//Add the product of direction and circleRadius to the circle's center position
+	circleCenter = circleCenter + ranDirection;
 }
+
+//MathLibrary::Vector2 WanderBehavior::setAngle(MathLibrary::Vector2 pos, int value)
+//{
+//	float x = rand() % (value * );
+//	float y = ;
+//}
 
 void WanderBehavior::update(Agent* agent, float deltatime)
 {
--- raygame/WanderBehavior.h
+++ raygame/WanderBehavior.h
@@ -5,14 +5,17 @@ class WanderBehavior : public SteeringBehavior
 {
 public:
 	WanderBehavior();
-	WanderBehavior(Agent* target, float wanderForce = 1);
+	WanderBehavior(float circleDistance = 1, float circleRadius = 1, float wanderForce = 1);
 
 	MathLibrary::Vector2 calculateForce(Agent* agent) override;
+	MathLibrary::Vector2 setAngle(MathLibrary::Vector2 point, int value);
 
 	void update(Agent* agent, float deltatime) override;
 	void draw(Agent* agent) override;
 private:
 	Actor* m_target;
 	float m_wanderAngle;
+	float m_circleDistance;
+	float m_circleRadius;
 };
 
