3d19acf23bc9f3378b37042e0474c551da518dab
Ben Brownlie <benjibspam@gmail.com>
2021-03-17 10:16:15 -0500

    Added Node and Edge classes
    Added Node and Edge classes and defined them.

--- raygame/Game.cpp
+++ raygame/Game.cpp
@@ -67,10 +67,7 @@ void Game::start()
 
 void Game::update(float deltaTime)
 {
-	for (int i = 0; i < m_sceneCount; i++)
-	{
-		m_scenes[i]->update(deltaTime);
-	}
+	getCurrentScene()->update(deltaTime);
 }
 
 void Game::draw()
@@ -80,10 +77,7 @@ void Game::draw()
 	BeginMode2D(*m_camera);
 	ClearBackground(BLACK);
 
-	for (int i = 0; i < m_sceneCount; i++)
-	{
-		m_scenes[i]->draw();
-	}
+	getCurrentScene()->draw();
 
 	EndMode2D();
 	EndDrawing();
---
+++ raygame/Node.cpp
@@ -0,0 +1,24 @@
+#include "Node.h"
+#include "Edge.h"
+#include <raylib.h>
+
+Node::Node(int x, int y, int nodeSize)
+{
+	graphPosition.x = x;
+	graphPosition.y = y;
+	size = nodeSize;
+}
+
+void Node::draw()
+{
+	//Draw a circle that represents the node
+	DrawCircle(getWorldPosition().x * 32, getWorldPosition().y * 32, size, GetColor(color));
+
+	//Draw all edges connected to this node
+	for (int i = 0; i < edges.size(); i++)
+	{
+		//Checks if the node is the first connected node to prevent the edge from drawing twice
+		if (edges[i]->connectedNode1->graphPosition == graphPosition)
+			edges[i]->draw();
+	}
+}
